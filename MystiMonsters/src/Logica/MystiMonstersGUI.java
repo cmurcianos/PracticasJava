1891efd7e9940d6750c6f261d3d240d74f0dbb2f
cmurcianos <147940917+cmurcianos@users.noreply.github.com>
2023-10-15 12:07:44 +0200

    Update MystiMonstersGUI.java

--- MystiMonsters/src/Logica/MystiMonstersGUI.java
+++ MystiMonsters/src/Logica/MystiMonstersGUI.java
@@ -3,6 +3,8 @@ package Logica;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MystiMonstersGUI extends JFrame {
 
@@ -11,30 +13,41 @@ public class MystiMonstersGUI extends JFrame {
     private int entrenadorX; // Posición X del personaje
     private int entrenadorY; // Posición Y del personaje
 
+    private final int mapaAncho = 400; // Ancho del mapa
+    private final int mapaAlto = 600; // Alto del mapa
+
+    private List<String> mapas; // Lista de nombres de mapas
+    private int mapaActual; // Índice del mapa actual
+
     public MystiMonstersGUI() {
         setTitle("MystiMonsters Game");
-        setSize(800, 600);
+        setSize(mapaAncho, mapaAlto);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+
+        // Inicializa la lista de mapas y el índice del mapa actual
+        mapas = new ArrayList<>();
+        mapas.add("resources/mapa2.png");
+        mapas.add("resources/maparocapeque.png");
+        mapaActual = 0;
 
         // Inicializa la posición del personaje
         entrenadorX = 100;
         entrenadorY = 400;
 
-        // Configura el fondo
-        background = new JLabel(new ImageIcon("resources/mapa2.png"));
-        background.setBounds(0, 0, 800, 600);
+        // Configura el fondo con el mapa actual
+        background = new JLabel(new ImageIcon(mapas.get(mapaActual)));
+        background.setBounds(0, 0, mapaAncho, mapaAlto);
 
         // Configura el personaje "Entrenador" y hazlo más pequeño
         entrenador = new JLabel(new ImageIcon("resources/entrenador.png"));
         entrenador.setBounds(entrenadorX, entrenadorY, 50, 50); // Cambia el tamaño a 50x50
 
-        // Agrega el fondo al contenedor primero para que esté en la parte posterior
+        // Agrega el fondo al contenedor primero
         getContentPane().add(background);
         // Luego, agrega el personaje para que esté en la parte delantera
-        getContentPane().add(entrenador,0);
-        
-        
+        getContentPane().add(entrenador, 0);
+
         setLayout(null);  // Usamos un diseño absoluto para posicionar los componentes
         setVisible(true);
 
@@ -52,19 +65,37 @@ public class MystiMonstersGUI extends JFrame {
 
                 if (keyCode == KeyEvent.VK_LEFT) {
                     // Mover el personaje hacia la izquierda
-                    entrenadorX -= step;
+                    if (entrenadorX - step >= 0) {
+                        entrenadorX -= step;
+                    }
                 } else if (keyCode == KeyEvent.VK_RIGHT) {
                     // Mover el personaje hacia la derecha
-                    entrenadorX += step;
+                    if (entrenadorX + step + 50 <= mapaAncho) {
+                        entrenadorX += step;
+                    }
                 } else if (keyCode == KeyEvent.VK_UP) {
                     // Mover el personaje hacia arriba
-                    entrenadorY -= step;
+                    if (entrenadorY - step >= 0) {
+                        entrenadorY -= step;
+                    }
                 } else if (keyCode == KeyEvent.VK_DOWN) {
                     // Mover el personaje hacia abajo
-                    entrenadorY += step;
+                    if (entrenadorY + step + 50 <= mapaAlto) {
+                        entrenadorY += step;
+                    }
                 }
 
-                // Actualizar la posición del personaje
+                // Verifica si el personaje ha llegado al límite del mapa
+                if (entrenadorX >= 350) {
+                    if (mapaActual < mapas.size() - 1) {
+                        // Cambia al siguiente mapa de la lista
+                        mapaActual++;
+                        background.setIcon(new ImageIcon(mapas.get(mapaActual)));
+                        entrenadorX = 100; // Reinicia la posición del personaje
+                    }
+                }
+
+                // Actualiza la posición del personaje
                 entrenador.setBounds(entrenadorX, entrenadorY, 50, 50);
             }
 
@@ -84,4 +115,5 @@ public class MystiMonstersGUI extends JFrame {
         });
     }
 }
+
 
