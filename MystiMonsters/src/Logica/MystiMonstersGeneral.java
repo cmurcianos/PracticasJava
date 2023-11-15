2cdeb26e86bae058a1dc708d67d5c57c0691c0d1
cmurcianos <147940917+cmurcianos@users.noreply.github.com>
2023-11-15 18:02:02 +0100

    Update MystiMonstersGeneral.java

--- MystiMonsters/src/Logica/MystiMonstersGeneral.java
+++ MystiMonsters/src/Logica/MystiMonstersGeneral.java
@@ -2,17 +2,15 @@ package Logica;
 
 
 public abstract class MystiMonstersGeneral {
-	
-	protected int numpokedex;
-	protected String nombre;
-	protected double peso;
-	protected String sexo;
-	protected int temporada;
-	
-	protected abstract void atacarPlacaje();
-	protected abstract void atacarAraniazo();
-	protected abstract void atacarMordisco();
-	
-	
-	
+
+    protected int nummystimonster;
+    protected String nombre;
+    protected double peso;
+    protected String sexo;
+
+
+    public MystiMonstersGeneral(String nombre, int salud, int ataque, int defensa) {
+
+    }
 }
+
