0b79b6e98b72dcd12f6d58667dd9713362939f6f
cmurcianos <147940917+cmurcianos@users.noreply.github.com>
2023-11-24 18:05:31 +0100

    Update MystiMonstersGeneral.java

--- MystiMonsters/src/Logica/MystiMonstersGeneral.java
+++ MystiMonsters/src/Logica/MystiMonstersGeneral.java
@@ -1,16 +1,37 @@
 package Logica;
-
-
-public abstract class MystiMonstersGeneral {
-
-    protected int nummystimonster;
-    protected String nombre;
-    protected double peso;
-    protected String sexo;
-
+class MystiMonstersGeneral {
+    private String nombre;
+    private int salud;
+    private int ataque;
+    private int defensa;
 
     public MystiMonstersGeneral(String nombre, int salud, int ataque, int defensa) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.ataque = ataque;
+        this.defensa = defensa;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void recibirDano(int dano) {
+        salud -= dano;
+        if (salud < 0) {
+            salud = 0;
+        }
     }
 }
-
