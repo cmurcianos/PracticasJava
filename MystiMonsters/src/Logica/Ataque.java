d1907eae07746fc144d83ac0116bf5f99ed34413
cmurcianos <147940917+cmurcianos@users.noreply.github.com>
2023-11-24 18:06:51 +0100

    Create Ataque.java

---
+++ MystiMonsters/src/Logica/Ataque.java
@@ -0,0 +1,14 @@
+package Logica;
+public class Ataque {
+    private String nombre;
+    private int poder;
+
+    public Ataque(String nombre, int poder) {
+        this.nombre = nombre;
+        this.poder = poder;
+    }
+
+    public int getPoder() {
+        return poder;
+    }
+}
