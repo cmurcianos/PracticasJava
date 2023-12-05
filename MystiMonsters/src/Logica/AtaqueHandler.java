e26955b6b435b979caf6ded230fb32f646d0e7ff
cmurcianos <147940917+cmurcianos@users.noreply.github.com>
2023-12-05 17:08:26 +0100

    Update and rename Ataque.java to AtaqueHandler.java

---
+++ MystiMonsters/src/Logica/AtaqueHandler.java
@@ -0,0 +1,20 @@
+package Logica;
+
+public class AtaqueHandler {
+
+    public static void ejecutarAtaque(Entrenador entrenador, Mochila mochila) {
+        // Simulate attack logic
+        // You can customize this logic based on your game requirements
+        System.out.println("Ataque mortal. Monstruo destruido y añadido a la mochila");
+
+        // Assume you have a Monstruo object that represents the defeated Monstruo
+        Monstruo monstruo;
+        monstruo = new Monstruo("Defeated Monstruo");
+
+        // Add the defeated Monstruo to the mochila
+        mochila.agregarMonstruo(monstruo);
+
+        // Move the entrenador back to the mapa principal
+        entrenador.setLocation(mochila.MAPA_PRINCIPAL);
+    }
+}
