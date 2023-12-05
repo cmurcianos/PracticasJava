60d0b5807c74cb3d2a0f8f62ac3923b6671321cf
cmurcianos <147940917+cmurcianos@users.noreply.github.com>
2023-12-05 17:26:34 +0100

    Update BarraDeVidaDialog.java

--- MystiMonsters/src/Logica/BarraDeVidaDialog.java
+++ MystiMonsters/src/Logica/BarraDeVidaDialog.java
@@ -1,4 +1,8 @@
-
+/**
+ * La clase BarraDeVidaDialog representa un cuadro de diálogo que muestra la barra de vida de un Monstruo.
+ * Este cuadro de diálogo incluye el nombre del Monstruo, una barra de progreso que representa la vida actual del Monstruo
+ * y un botón para cerrar el cuadro de diálogo.
+ */
 package Logica;
 
 import javax.swing.*;
@@ -6,6 +10,12 @@ import java.awt.*;
 
 public class BarraDeVidaDialog extends JDialog {
 
+    /**
+     * Crea un nuevo BarraDeVidaDialog.
+     *
+     * @param parent   El JFrame padre al que se asocia el cuadro de diálogo.
+     * @param monstruo El Monstruo cuya barra de vida se muestra en el cuadro de diálogo.
+     */
     public BarraDeVidaDialog(JFrame parent, Monstruo monstruo) {
         super(parent, "Barra de Vida", true);
         setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
@@ -13,14 +23,18 @@ public class BarraDeVidaDialog extends JDialog {
         setLocationRelativeTo(parent);
 
         JPanel panel = new JPanel(new BorderLayout());
+
+        // Etiqueta que muestra el nombre del Monstruo
         JLabel label = new JLabel("Vida de " + monstruo.getNombre());
         panel.add(label, BorderLayout.NORTH);
 
+        // Barra de progreso que representa la vida actual del Monstruo
         JProgressBar progressBar = new JProgressBar(0, monstruo.getVidaMaxima());
         progressBar.setValue(monstruo.getVidaActual());
         progressBar.setStringPainted(true);
         panel.add(progressBar, BorderLayout.CENTER);
 
+        // Botón para cerrar el cuadro de diálogo
         JButton cerrarButton = new JButton("Cerrar");
         cerrarButton.addActionListener(e -> dispose());
         panel.add(cerrarButton, BorderLayout.SOUTH);
