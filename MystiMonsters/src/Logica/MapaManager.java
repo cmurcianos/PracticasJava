e27f2fa7c277c1fc0468cc1c407fca056869d1c0
cmurcianos <147940917+cmurcianos@users.noreply.github.com>
2023-12-05 17:14:32 +0100

    Update and rename Monstruitos.java to MapaManager.java

---
+++ MystiMonsters/src/Logica/MapaManager.java
@@ -0,0 +1,14 @@
+
+package Logica;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class MapaManager {
+    // Puedes agregar lógica específica del mapa aquí
+
+    public void drawMapSpecificImages(Graphics g, BufferedImage backgroundImage) {
+        // Lógica para dibujar imágenes específicas en el mapa
+        g.drawImage(backgroundImage, 0, 0, null);
+    }
+}
