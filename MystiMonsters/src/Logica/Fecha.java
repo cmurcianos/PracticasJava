8d256120e36a6caab333833bb62222b35aa9d44a
cmurcianos <147940917+cmurcianos@users.noreply.github.com>
2023-12-05 17:42:29 +0100

    Update Fecha.java

--- MystiMonsters/src/Logica/Fecha.java
+++ MystiMonsters/src/Logica/Fecha.java
@@ -1,13 +1,25 @@
-
+/**
+ * La clase Flecha representa una flecha en el contexto del juego. La flecha tiene una posición
+ * (coordenadas x, y), un tamaño, y una dirección en la que apunta.
+ */
 package Logica;
 
 import java.awt.*;
 
 public class Flecha {
-    private int x, y;
-    private int size;
-    private Direccion direccion;
 
+    private int x, y; // Posición de la flecha
+    private int size; // Tamaño de la flecha
+    private Direccion direccion; // Dirección en la que apunta la flecha
+
+    /**
+     * Crea una nueva instancia de la clase Flecha.
+     *
+     * @param x         La coordenada x de la posición inicial de la flecha.
+     * @param y         La coordenada y de la posición inicial de la flecha.
+     * @param size      El tamaño de la flecha.
+     * @param direccion La dirección en la que apunta la flecha.
+     */
     public Flecha(int x, int y, int size, Direccion direccion) {
         this.x = x;
         this.y = y;
@@ -15,6 +27,11 @@ public class Flecha {
         this.direccion = direccion;
     }
 
+    /**
+     * Dibuja la flecha en la posición especificada.
+     *
+     * @param g El objeto Graphics utilizado para dibujar la flecha.
+     */
     public void dibujar(Graphics g) {
         // Implementa la lógica para dibujar la flecha en la posición (x, y)
         g.setColor(Color.BLACK);
@@ -49,19 +66,28 @@ public class Flecha {
         g.fillPolygon(xPoints, yPoints, 3);
     }
 
+    /**
+     * Verifica si el entrenador está en contacto con la flecha.
+     *
+     * @param entrenadorX      La coordenada x del entrenador.
+     * @param entrenadorY      La coordenada y del entrenador.
+     * @param entrenadorWidth  El ancho del entrenador.
+     * @param entrenadorHeight La altura del entrenador.
+     * @return true si el entrenador está en contacto con la flecha, false de lo contrario.
+     */
     public boolean entrenadorEnContacto(int entrenadorX, int entrenadorY, int entrenadorWidth, int entrenadorHeight) {
-        // Verifica si el entrenador está en contacto con la flecha
         return entrenadorX < x + size &&
                 entrenadorX + entrenadorWidth > x &&
                 entrenadorY < y + size &&
                 entrenadorY + entrenadorHeight > y;
     }
 
+    /**
+     * Obtiene la dirección en la que apunta la flecha.
+     *
+     * @return La dirección de la flecha.
+     */
     public Direccion getDireccion() {
         return direccion;
     }
-
-
 }
-
-
